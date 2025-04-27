b271324d6ac121e79f86d01f286e5508b68228a0
adieptr <adie6809@gmail.com>
2025-04-27 15:18:40 +0700

    perbaiki landing page 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -624,7 +624,7 @@
                                     <img src="{{ asset('assets/images/buku6.png') }}" alt="Buku" class="img-fluid rounded shadow-sm" />
                                 </div>
                             </div>
-                            <!-- Tambahkan lebih banyak di sini jika ada -->
+
                         </div>
 
                         <!-- Tombol Lihat Semua -->
@@ -661,7 +661,6 @@
 
                 <section class="hubungi-kami" id="kontak">
                     <h2>Hubungi Kami</h2>
-                    <p class="subjudul">Di Sini</p>
                     <div class="kontainer-form">
                       <div class="gambar">
                         <img src="{{ asset('assets/images/tlpn.png') }}" alt="Telepon" />
@@ -792,35 +791,10 @@
                                 class="divider w-full flex border-top border-lightgray"></span></div>
                         <div class="col col-12 col-md-8">
                             <div class="ld-fancy-heading relative">
-                                <p class="ld-fh-element mb-0/5em inline-block relative text-12 leading-1/75em">These
-                                    Terms
-                                    will be applied fully and affect to your use of this Website. By using this Website,
-                                    you
-                                    agreed to accept all terms and conditions written in here. You must not use this
-                                    Website if
-                                    you disagree with any of these Website Standard Terms and Conditions.</p>
                             </div>
                         </div>
                         <div class="col col-12 col-md-4">
-                            <div class="flex flex-row justify-end items-center gap-10 sm:justify-start"><a
-                                    class="icon social-icon animation-grow mr-25 text-24 w-25 h-25 leading-24"
-                                    href="#" target="_blank"><span class="sr-only">Instagram</span>
-                                    <svg class="w-1em h-1em relative block" viewbox="0 0 448 512"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
-                                            fill="#5F7A9E"></path>
-                                    </svg>
-                                </a><a class="icon social-icon animation-grow text-24 w-25 h-25 leading-24"
-                                    href="#" target="_blank"><span class="sr-only">Twitter</span>
-                                    <svg class="w-1em h-1em relative block" viewbox="0 0 512 512"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
-                                            fill="#5F7A9E"></path>
-                                    </svg>
-                                </a></div>
-                        </div>
+
                     </div>
                 </div>
             </section>
