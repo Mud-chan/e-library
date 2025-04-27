3f84b8703d0998843daa35e9aa3746eb63fbc161
adieptr <adie6809@gmail.com>
2025-04-27 15:15:03 +0700

    perbaiki landing page 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -130,8 +130,8 @@
         }
 
         .gambar img {
-            width: 225px; /* Sesuaikan dengan lebar ideal */
-            height: 325px; /* Tinggi lebih besar, seperti bentuk portrait */
+            width: 225px;
+            height: 325px;
             border-radius: 20px;
             object-fit: cover;
             display: block;
@@ -205,9 +205,9 @@
                         </div>
                         <div class="navbar-brand-plain py-20 xxl:hidden xl:hidden sm:flex"><a
                                 class="navbar-brand flex p-0 relative" href="/" rel="home"><span
-                                    class="navbar-brand-inner post-rel"><img alt="StartInaama" class="logo-sticky"
+                                    class="navbar-brand-inner post-rel"><img alt="image" class="logo-sticky"
                                         src="assets/images/demo/start-hub-2/logo/logo-mob-d.svg">
-                                    <img alt="StartInaama" class="logo-default"
+                                    <img alt="image" class="logo-default"
                                         src="assets/images/demo/start-hub-2/logo/logo.png"></span></a>
                         </div>
                     </div>
@@ -238,36 +238,7 @@
                     </div>
                     <div class="w-25percent flex items-center justify-end mr-20 lg:w-60percent lg:mr-0">
                         <div class="flex items-center justify-end">
-                            <div class="module-social lg:hidden">
-                                <div class="social-icons-wrapper justify-end"><span class="grid-item"><a
-                                            class="icon social-icon animation-pulse-grow items-center text-15 text-white-30"
-                                            href="#" target="_blank"><span class="sr-only">Facebook</span> <svg
-                                                class="text-15" fill="currentColor" height="1em"
-                                                viewbox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z">
-                                                </path>
-                                            </svg> </a></span><span class="grid-item"><a
-                                            class="icon social-icon animation-pulse-grow items-center text-15 text-white-30"
-                                            href="#" target="_blank"><span class="sr-only">Twitter</span> <svg
-                                                class="text-15" fill="currentColor" height="1em"
-                                                viewbox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z">
-                                                </path>
-                                            </svg> </a></span><span class="grid-item"><a
-                                            class="icon social-icon animation-pulse-grow items-center text-15 text-white-30"
-                                            href="#" target="_blank"><span class="sr-only">Github</span> <svg
-                                                class="text-15" fill="currentColor" height="1em"
-                                                viewbox="0 0 496 512" xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z">
-                                                </path>
-                                            </svg></a></span>
-                                </div>
-                            </div>
-                            {{-- <a class="btn btn-solid text-white bg-gray-600 rounded-100 ml-10 text-15 font-medium bg-bg hover:text-black hover:bg-white module-btn-sm w-200"
-                                href="/logreg"><span class="btn-txt" data-text="Login">Masuk/Daftar</span></a> --}}
+
                             <div class="ml-15 ld-module-sd ld-module-sd-hover ld-module-sd-right xxl:hidden lg:block">
                                 <button aria-expanded="false"
                                     class="bg-transparent border-none nav-trigger flex relative items-center justify-center style-6 collapsed"
@@ -299,14 +270,15 @@
                                                                         class="lqd-icn-ess icon-ion-ios-arrow-down"></i></span></a>
                                                         </li>
                                                         <li><a class="w-full text-20 text-black font-medium leading-1/5em"
-                                                                href="#about"><span>Why Us?</span> <span
+                                                                href="#about"><span>Keunggulan</span> <span
                                                                     class="link-icon inline-flex hide-if-empty right-icon"><i
                                                                         class="lqd-icn-ess icon-ion-ios-arrow-down"></i></span></a>
                                                         </li>
                                                         <li class="menu-item-has-children"><a
                                                                 class="w-full text-20 text-black font-medium leading-1/5em"
                                                                 href="#koleksi"><span>Koleksi Buku</span> <span
-                                                                    class="submenu-expander absolute inline-flex right-0"></span></a>
+                                                                class="link-icon inline-flex hide-if-empty right-icon"><i
+                                                                    class="lqd-icn-ess icon-ion-ios-arrow-down"></i></span></a>
                                                         </li>
                                                         <li><a class="w-full text-20 text-black font-medium leading-1/5em"
                                                                 href="#informasi"><span>Informasi</span> <span
@@ -321,7 +293,7 @@
                                                     </ul>
                                                 </div>
                                             </div>
-                                            <div class="flex justify-start mt-25 gap-25"><a
+                                            {{-- <div class="flex justify-start mt-25 gap-25"><a
                                                     class="icon social-icon social-icon-facebook animation-pulse-grow text-26 w-25"
                                                     href="#" target="_blank"><span
                                                         class="sr-only">Facebook</span>
@@ -352,7 +324,7 @@
                                                         </path>
                                                     </svg>
                                                 </a></div>
-                                        </div>
+                                        </div> --}}
                                     </div>
                                 </div>
                                 <div class="lqd-module-backdrop"></div>
@@ -382,7 +354,7 @@
         </header>
         <main class="content">
             <div id="lqd-contents-wrap">
-                <section class="lqd-section banner pt-140 pb-200 transition-all bg-transparent text-white-50 relative"
+                <section class="lqd-section banner pt-140 pb-400 transition-all bg-transparent text-white-50 relative"
                     id="banner" style="background-image: linear-gradient(120deg, #62E19F 0%, #2BCA77 100%);">
                     <div class="lqd-shape lqd-shape-bottom" data-negative="false">
                         <svg class="lqd-custom-shape" fill="none" height="461" preserveaspectratio="none"
@@ -549,10 +521,7 @@
                                         <div class="contents">
                                             <h3 class="text-20 mb-0/5em lqd-iconbox-heading">Akses 24 Jam Baca Buku
                                             </h3>
-                                            <p>Lorem Ipsum Dolor Sit Amet Constetetur Adipiscing
-                                                sit amet Lorem Ipsum Dolor Sit  Constetetur Adipiscing
-                                                adipiscing Dolor Lorem Ipsum Dolor Sit Adipiscing
-                                            <p>
+                                            <p>Dengan platform E-Library, kamu bisa membaca ribuan koleksi buku hanya dengan satu klik. Tidak perlu repot membawa buku fisik, cukup buka website kapan saja, di mana saja, lewat smartphone, tablet, atau laptop. Fleksibel untuk semua kebutuhan belajar dan hiburanmu, tanpa batasan waktu.<p>
                                         </div>
                                     </div>
                                 </div>
@@ -580,10 +549,8 @@
                                             </div>
                                         </div>
                                         <div class="contents">
-                                            <h3 class="text-20 mb-0/5em lqd-iconbox-heading">Meingkatkan Minat Baca</h3>
-                                            <p>Lorem ipsum dolor sit amet kolo jiro mun
-                                                hin gurt binstre loperz nuklin hedore setaf
-                                                bulk in jownline jertux zerin mein aneen  </p>
+                                            <h3 class="text-20 mb-0/5em lqd-iconbox-heading">Meningkatkan Minat Baca</h3>
+                                            <p>Website E-Library kami dirancang untuk membuat pengalaman membaca menjadi lebih menarik dan menyenangkan. Dengan tampilan yang user-friendly, rekomendasi buku yang dipersonalisasi, serta fitur untuk memberikan ulasan, kami berkomitmen untuk membangun budaya membaca di era yang serba digital ini.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -611,8 +578,7 @@
                                         <div class="contents">
                                             <h3 class="text-20 mb-0/5em lqd-iconbox-heading">Simpan Buku yang Disukai
                                             </h3>
-                                            <p>Lorem ipsum asik sit amet menjadi wey
-                                                aku bisa di  apaan tuh bisa gitu anjay</p>
+                                            <p>Tak perlu khawatir kehilangan buku favorit! Di E-Library, kamu bisa menyimpan buku-buku pilihan ke dalam koleksi pribadi. Kelola daftar bacaanmu, tandai buku yang ingin dibaca, dan lanjutkan membaca kapan pun kamu mau. Semua dalam satu platform yang mudah diakses dan nyaman.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -1075,21 +1041,10 @@
         </div>
     </div>
     <div class="lqd-cc lqd-cc--outer fixed top-0 left-0 pointer-events-none"></div>
-    {{-- <Inaama id="lqd-temp-snickersbar">
-        <div class="lqd-snickersbar flex flex-wrap lqd-snickersbar-in" data-item-id="">
-            <div class="lqd-snickersbar-inner flex flex-wrap items-center">
-                <div class="lqd-snickersbar-detail"><p class="lqd-snickersbar-addding-temp mt-0 mb-0 hidden">Adding
-                {{itemName}} to cart</p>
-                <p class="lqd-snickersbar-added-temp mt-0 mb-0 hidden">Added {{itemName}} to cart</p>
-                <p class="lqd-snickersbar-msg flex items-center mt-0 mb-0"></p>
-                <p class="lqd-snickersbar-msg-done flex items-center mt-0 mb-0"></p></div>
-            <div class="lqd-snickersbar-ext ml-1/5em"></div>
-            </div>
-        </div>
-    </Inaama> --}}
-    <Inaama id="lqd-temp-sticky-header-sentinel">
+
+    <elemen id="lqd-temp-sticky-header-sentinel">
         <div class="lqd-sticky-sentinel invisible absolute pointer-events-none"></div>
-    </Inaama>
+    </elemen>
     <div aria-label="Dialog Window (Press escape to close)" class="lity hidden" data-modal-type="default"
         role="dialog" tabindex="-1">
         <div class="lity-backdrop"></div>
