930dac0281965ab305b7dcb4cd4c30d4bc04593c
pramudya <e41220957@student.polije.ac.id>
2025-05-10 01:18:23 +0700

    memperbaiki script tutup button

--- public/assets/js/admin_script.js
+++ public/assets/js/admin_script.js
@@ -138,26 +138,23 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-function closeModalAndClearSession() {
-    // Menghilangkan modal
-    document.getElementById('gagal-message').style.display = 'none';
+// function closeModalAndClearSession() {
+//     document.getElementById('gagal-message').style.display = 'none';
 
-    // Mengirim permintaan HTTP untuk menghapus session
-    fetch('/clear-session', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-TOKEN': '{{ csrf_token() }}'
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Failed to clear session');
-        }
-        // Jika berhasil, perbarui halaman
-        location.reload();
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+//     fetch('/clear-session', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'X-CSRF-TOKEN': '{{ csrf_token() }}'
+//         }
+//     })
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Failed to clear session');
+//         }
+//         location.reload();
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+// }
