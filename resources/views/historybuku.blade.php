0da1fd62cc443cd026e5e3e62e7e603e8f204bdb
pramudya <e41220957@student.polije.ac.id>
2025-05-21 10:37:54 +0700

    menambahkan tombol topsis untuk menu histori siswa

--- resources/views/historybuku.blade.php
+++ resources/views/historybuku.blade.php
@@ -38,6 +38,7 @@
             <a href="{{ url('/profilesiswa') }}" class="btn">View Profile</a>
             <a href="{{ url('/historybuku') }}" class="btn">History</a>
             <a href="{{ url('/bookmarkbuku') }}" class="btn">Bookmark</a>
+            <a href="{{ url('/recommend') }}" class="btn">SPK TOPSIS</a>
 
             <a href="{{ route('logoutsiswa') }}" onclick="return confirm('Anda Yakin Ingin Logout?');"
             class="delete-btn">log out</a>
