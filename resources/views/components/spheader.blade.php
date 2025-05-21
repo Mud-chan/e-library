072f6430ba808cfa140bf133e2052e3770a06b15
pramudya <e41220957@student.polije.ac.id>
2025-05-21 11:32:46 +0700

    menghapus menu informasi pada guru dan admin

--- resources/views/components/spheader.blade.php
+++ resources/views/components/spheader.blade.php
@@ -77,7 +77,7 @@
             <a href="{{ route('contentsp.index') }}"><i class="fa-solid fa-book"></i><span>Buku</span></a>
             <a href="{{ route('tutor.index') }}"><i class="fa-solid fa-user-graduate"></i><span>Guru</span></a>
             <a href="{{ route('siswa.index') }}"><i class="fas fa-graduation-cap"></i><span>Siswa</span></a>
-            <a href=""><i class="fa-solid fa-circle-exclamation"></i><span>Informasi</span></a>
+            
             {{-- <a href=""><i class="fa-solid fa-circle-dollar-to-slot"></i><span>Peminjaman</span></a> --}}
             <a href="{{ route('logoutsp') }}" onclick="return confirm('Anda Yakin Ingin Logout?');"><i class="fas fa-right-from-bracket"></i><span>Log out</span></a>
 
